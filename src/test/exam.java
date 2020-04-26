a9542b196d79f0492c5bfe7662b21768843c760d
BMaMan <lsm4103@gmail.com>
2020-04-27 01:25:20 +0900

    first commit

---
+++ src/test/exam.java
@@ -0,0 +1,14 @@
+package test;
+import java.util.Scanner;
+
+public class exam {
+	public static void main(String[] args) {
+		int a, b;
+		
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a + b);
+	}
+}
